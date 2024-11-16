80279a7c9d2655e72568871d8f36ea643a7d0d90
kyivskyi-knyaz <aleksandrmashtein@gmail.com>
2024-11-16 03:17:43 +0200

    Revert "gubnoy hash mappp"

--- build.sh
+++ build.sh
@@ -1,4 +1,1 @@
-#g++ -O3 -Wall -g -c -std=c++17 -fsanitize=signed-integer-overflow main.cpp -o main.o
-#gcc -O3 -Wall -g -c hash.h -o hash.o
-gcc -o myprog main.cpp -lstdc++ 
-
+g++ -O3 -Wall -g -std=c++17 -fsanitize=signed-integer-overflow main.cpp -o my_program
